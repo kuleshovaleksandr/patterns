8d4abb50bab89d4b7b785eba7a3af27dd19ef6ae
kuleshovaleksandr <drevenb@gmail.com>
2023-03-27 20:56:18 +0300

    Add facade pattern

---
+++ src/main/java/patterns/structural/facade/MPEG4CompressionCodec.java
@@ -0,0 +1,5 @@
+package main.java.patterns.structural.facade;
+
+public class MPEG4CompressionCodec implements Codec {
+    public String type = "mp4";
+}
---
+++ src/main/java/patterns/structural/facade/OggCompressionCodec.java
@@ -0,0 +1,5 @@
+package main.java.patterns.structural.facade;
+
+public class OggCompressionCodec implements Codec {
+    public String type = "ogg";
+}
