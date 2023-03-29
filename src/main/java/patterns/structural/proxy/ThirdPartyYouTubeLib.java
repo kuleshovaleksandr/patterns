53dcd227474f22e87b3783d70b1343f4ae46b579
kuleshovaleksandr <drevenb@gmail.com>
2023-03-29 18:35:46 +0300

    Add proxy pattern

---
+++ src/main/java/patterns/structural/proxy/ThirdPartyYouTubeLib.java
@@ -0,0 +1,9 @@
+package main.java.patterns.structural.proxy;
+
+import java.util.HashMap;
+
+public interface ThirdPartyYouTubeLib {
+    HashMap<String, Video> popularVideos();
+
+    Video getVideo(String videoId);
+}
