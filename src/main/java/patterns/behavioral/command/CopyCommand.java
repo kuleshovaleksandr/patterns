3ccaec5745bcff6006787e574a4be85ec2777055
kuleshovaleksandr <drevenb@gmail.com>
2023-03-30 21:52:38 +0300

    Add command pattern

---
+++ src/main/java/patterns/behavioral/command/CopyCommand.java
@@ -0,0 +1,13 @@
+package main.java.patterns.behavioral.command;
+
+public class CopyCommand extends Command {
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
