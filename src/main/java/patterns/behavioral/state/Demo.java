d6255f233d9fe5f8f4214e0a16761ce963436855
drevenb <drevenb@gmail.com>
2023-04-04 10:27:57 +0300

    Add state pattern

---
+++ src/main/java/patterns/behavioral/state/Demo.java
@@ -0,0 +1,9 @@
+package main.java.patterns.behavioral.state;
+
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
---
+++ src/main/java/patterns/behavioral/state/State.java
@@ -0,0 +1,18 @@
+package main.java.patterns.behavioral.state;
+
+public abstract class State {
+    Player player;
+
+    /**
+     * Контекст передаёт себя в конструктор состояния, чтобы состояние могло
+     * обращаться к его данным и методам в будущем, если потребуется.
+     */
+    State(Player player) {
+        this.player = player;
+    }
+
+    public abstract String onLock();
+    public abstract String onPlay();
+    public abstract String onNext();
+    public abstract String onPrevious();
+}
